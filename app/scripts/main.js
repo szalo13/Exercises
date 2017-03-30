a1c684a607a399b08bc61b76df72abcf55f7f14b
szalo13 <kamil_sza@yahoo.pl>
2017-03-30 18:24:27 +0200

    Adding revert word script with cutting existing letters

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
 console.log('\'Allo \'Allo!');
+
+function showFromBehind(testWord) {
+
+  var finalWord = "";
+  var letter = "";
+
+  for (var i = testWord.length; i > 0; i--) {
+    letter = testWord.charAt(i-1);
+
+    if(finalWord.indexOf(letter) < 0) {
+      finalWord = finalWord.concat(letter);
+    } else {
+      // letter exist in a word
+    }
+  }
+
+  alert(finalWord);
+}
